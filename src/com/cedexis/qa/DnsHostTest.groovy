ef69e79eaa83624dd595b6a61d0fe6f532d4c960
skylar.hiebert@gmail.com <skylar.hiebert@webtrends.com>
2014-12-22 16:15:16 -0800

    Added basic wireframe for beginning tests"

--- src/com/cedexis/qa/DnsHostTest.groovy
+++ src/com/cedexis/qa/DnsHostTest.groovy
@@ -1,9 +1,43 @@
 package com.cedexis.qa
 
+import net.sf.json.JSONArray
+import org.testng.annotations.*
+import groovyx.net.http.ContentEncoding
+import groovyx.net.http.HTTPBuilder
+import groovy.util.logging.Log4j
+
 /**
  * Created by hieberts on 12/22/14.
  */
 class DnsHostTest {
 
-    
+    @DataProvider(name = "hostsAndServices")
+    Object[][] hostsAndServices() { [
+        ["testhost", ""]
+    ] }
+
+    @Test
+    testVerifyLookup(String queryHost, String serviceHost) {
+        def httpBuilder = new HTTPBuilder()
+
+        http.request(, GET, JSON) {
+            uri.path = "/apps"
+
+            response.failure = { resp ->
+                assert false
+            }
+
+            response.success = { resp, json ->
+                assert resp.statusLine.statusCode == HttpStatus.SC_OK
+            }
+        }
+    }
+
+    def makeAppsRequest(String queryHost, Closure callback) {
+        def httpBuilder = new HTTPBuilder("http://${queryHost}", JSONArray)
+    }
+
+    makeDnsLookup(String serviceHost, Closure callback) {
+
+    }
 }
